6aaab49e7e8f252db8b298dc814a79f78074d586
garzonax <garzonax@gmail.com>
2015-06-18 01:00:50 -0600

    consola

---
+++ src/laboratorio1/Consola.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio1;
+
+import static Observador.AgregarDato.id1;
+import static Observador.AgregarDato.sujeto;
+import Observador.ClI;
+import Observador.Jpa;
+import Observador.Observador;
+import Observador.Sujeto;
+import Observador.UI1;
+import java.util.Scanner;
+import persistence.Persona;
+
+
+/**
+ *
+ * @author Pablito Garzona
+ */
+public class Consola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String status;
+        String status1= new String("conectado");
+        String status2= new String("no conectado");
+        
+        Scanner in = new Scanner(System.in);
+        Sujeto sujeto = new Jpa();
+        Observador observador1= new ClI(sujeto);   
+       // Observador observador2= new UI1(sujeto);
+          
+        
+         sujeto.registrarObservador(observador1);
+        //sujeto.registrarObservador(observador2);
+          
+         // PARTE DE CONSOLA
+         
+               
+         
+         
+         Persona p= new Persona();
+         p.setId(id1);
+         System.out.println("BASE DE DATOS");
+         System.out.println("Ingrese Nombre");
+         p.setNombre(in.next());
+         System.out.println("Ingrese Apellido");
+         p.setApellido(in.next());
+         System.out.println("Ingrese Status");
+        status = in.next();
+         
+         
+         if (status.equals(status1))   {
+             p.setConectado(true);
+          }
+         if (status.equals(status2)){
+             p.setConectado(false);
+         }
+             
+         
+       /* p.setId(id1);
+        p.setNombre(texto1.getText());
+        p.setApellido(texto2.getText());
+        if(RadioBoton1.isSelected()){
+            p.setConectado(true); 
+        }
+        if(RadioBoton2.isSelected()){
+            p.setConectado(false); 
+        }
+        sujeto.Ingresar(p);
+        
+         */
+         
+    }
+    
+}
